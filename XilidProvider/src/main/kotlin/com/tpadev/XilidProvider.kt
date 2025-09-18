8ca079850e62202d5f52a5e3cbac060dc609995e
tpadev <69703841+tpadev@users.noreply.github.com>
2025-09-18 15:08:30 +0700

    Update XilidProvider.kt

--- XilidProvider/src/main/kotlin/com/tpadev/XilidProvider.kt
+++ XilidProvider/src/main/kotlin/com/tpadev/XilidProvider.kt
@@ -187,9 +187,9 @@ class XilidProvider : MainAPI() {
             Actor(it.select("meta[itemprop=name]").attr("content"), it.select("img").attr("src"))
         }
 
-        val recommendation = document.select("div.sbox.srelacionados a").mapNotNull {
+        val recommendation = document.select("div.sbox.srelacionados").mapNotNull {
     		val recName = it.selectFirst("img")?.attr("alt")?.replace(Regex("\\(\\d{4}\\)"), "")?.trim().toString() ?: return@mapNotNull null
-			val recHref = it.selectFirst.attr("href")? ?: return@mapNotNull null
+			val recHref = it.selectFirst("a")?attr("href") ?: return@mapNotNull null
     		val recPosterUrl = it.selectFirst("img")?.attr("src")
 			newMovieSearchResponse(
 				recName, 
