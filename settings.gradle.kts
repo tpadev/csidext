e183553eda57937054e72065324f3d2fd86b584d
tpadev <69703841+tpadev@users.noreply.github.com>
2025-10-01 23:09:18 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
 
 val disabled = listOf<String>("utils")
 //val disabled = listOf<String>()
-/*
+
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
         include(dir.name)
@@ -23,7 +23,7 @@ fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
 
-*/
+
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
 // include("PluginName")
-include("MBoxProvider")
+// include("MBoxProvider")
