2cc007aec95b6e20e08c585028a9cbb1ed6f2f1f
tpadev <69703841+tpadev@users.noreply.github.com>
2025-10-01 23:40:48 +0700

    Update MBoxProvider.kt

--- MBoxProvider/src/main/kotlin/com/tpadev/MBoxProvider.kt
+++ MBoxProvider/src/main/kotlin/com/tpadev/MBoxProvider.kt
@@ -274,19 +274,11 @@ class MBoxProvider : MainAPI() {
         val year = data.subject?.releaseDate?.getYear()
         val actors = data.stars
                     ?.filterNotNull()
-                    ?.map { star ->    
+                    ?.mapNotNull { star ->    
                         val name = star.name ?: return@map null
                         val actor = Actor(name, image = star.avatarUrl)
-                            if (!star.character.isNullOrBlank()) {
-                                // Actor with a role string
-                                actor to star.character
-                            } else {
-                                // Just the actor’s name (fallback)
-                                name
-                            }
-                    }
-                    ?.filterNotNull()
-                    ?: emptyList()
+                            actor to star.character
+                    } ?: emptyList()
         val tracker = if (data.subject?.countryName === "Japan")
             APIHolder.getTracker(
                 listOf(data.subject?.title ?: ""),
